4ae820dfc796e1975246693ec96538fb0c53d19c
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-02-04 11:28:24 +0800

    Update diy-part1.sh

--- SCRIPTS/R1S-H5/diy-part1.sh
+++ SCRIPTS/R1S-H5/diy-part1.sh
@@ -1,18 +1,2 @@
-#!/bin/bash
-#
-# Copyright (c) 2021 F-T-Otaku 
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/F-T-Otaku/Actions-ImmortalWrt-R1S-H5
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#git clone https://github.com/qzhome2102/luci-app-adblock-plus package/luci-app-adblock-plus
